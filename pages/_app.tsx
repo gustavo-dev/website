b8c7c262976d0f515aa1712c9d6a14b08f0d8e24
gustavo-dev <gvwchevrand@gmail.com>
2021-11-06 08:59:33 -0300

    feat: added suport for Sitches SSR & created GlobalStyles

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,30 @@
-import 'styles/globals.css';
-import type { AppProps } from 'next/app';
+import type { AppProps } from 'next/app'
+import { IdProvider } from '@radix-ui/react-id'
+import { globalCss } from '@stitches/react'
+import { darkTheme } from 'lib/stitches'
+
+const globalStyle = globalCss({
+    '*': {
+        margin: 0,
+        padding: 0,
+        fontFamily:
+            'IBM Plex Sans,ui-sans-serif,system-ui,-apple-system,BlinkMacSystemFont,Segoe UI,Roboto,Helvetica Neue,Arial,Noto Sans,sans-serif,Apple Color Emoji,Segoe UI Emoji,Segoe UI Symbol,Noto Color Emoji;',
+    },
+
+    button: {
+        border: 'none',
+    },
+})
 
 function MyApp({ Component, pageProps }: AppProps) {
-	return <Component {...pageProps} />;
+    globalStyle()
+
+    return (
+        <div className={darkTheme}>
+            <IdProvider>
+                <Component {...pageProps} />
+            </IdProvider>
+        </div>
+    )
 }
-export default MyApp;
+export default MyApp

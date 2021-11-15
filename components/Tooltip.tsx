03aa9c4031097e856c30f0219b1a2db511472256
gustavo-dev <gvwchevrand@gmail.com>
2021-11-15 16:32:37 -0300

    feat: created Tooltip component

---
+++ components/Tooltip.tsx
@@ -0,0 +1,37 @@
+import * as TooltipPrimitive from '@radix-ui/react-tooltip'
+import { ComponentDescription } from 'components/styled/Text'
+import { styled } from 'lib/stitches'
+
+interface TooltipProps {
+    trigger: JSX.Element
+}
+
+const StyledArrow = styled(TooltipPrimitive.Arrow, {
+    fill: '$tooltip',
+})
+
+const StyledContent = styled(TooltipPrimitive.Content, {
+    background: '$tooltip',
+
+    boxSizing: 'border-box',
+
+    br: 2,
+
+    p: '.8rem',
+
+    boxShadow: '0 1px 2px rgba(0, 0, 0, .4)',
+})
+
+export const Tooltip: React.FC<TooltipProps> = ({ trigger, children }) => {
+    return (
+        <TooltipPrimitive.Root delayDuration={350}>
+            <TooltipPrimitive.Trigger asChild>
+                {trigger}
+            </TooltipPrimitive.Trigger>
+            <StyledContent sideOffset={5}>
+                <ComponentDescription>{children}</ComponentDescription>
+                <StyledArrow />
+            </StyledContent>
+        </TooltipPrimitive.Root>
+    )
+}

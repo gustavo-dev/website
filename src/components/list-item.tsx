eae0646bffaf0fc47f23501cb48a56eabf72949b
gustavo-dev <gvwchevrand@gmail.com>
2022-07-24 19:20:38 -0300

    fix build

--- src/components/list-item.tsx
+++ src/components/list-item.tsx
@@ -1,13 +1,7 @@
 import { ReactNode } from 'react'
 import type { IconType } from 'react-icons'
 
-export default function ListItem({
-    text,
-    icon,
-}: {
-    text: ReactNode
-    icon: IconType
-}) {
+export function ListItem({ text, icon }: { text: ReactNode; icon: IconType }) {
     return (
         <li className="flex space-x-2">
             <span>{icon({ className: 'h-6 w-6' })}</span>

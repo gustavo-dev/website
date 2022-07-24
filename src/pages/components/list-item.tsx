fd610fd06bbc2d1fccdc95c5c959affc264d6808
gustavo-dev <gvwchevrand@gmail.com>
2022-07-24 18:59:05 -0300

    convert ListItem as default export

--- src/pages/components/list-item.tsx
+++ src/pages/components/list-item.tsx
@@ -1,7 +1,13 @@
 import { ReactNode } from 'react'
 import type { IconType } from 'react-icons'
 
-export function ListItem({ text, icon }: { text: ReactNode; icon: IconType }) {
+export default function ListItem({
+    text,
+    icon,
+}: {
+    text: ReactNode
+    icon: IconType
+}) {
     return (
         <li className="flex space-x-2">
             <span>{icon({ className: 'h-6 w-6' })}</span>
